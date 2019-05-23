7337894e71f1515039dc39a50a5a7b1c194b40ba
ekvachuk <jekvach@ukr.net>
2019-05-23 19:40:59 +0300

    Initial commit

---
+++ src/org/itstep/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package org.itstep;
+
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        someMethod("Hi! My name is Evgen");
+    }
+
+    static void someMethod(String msg) {
+        System.out.println(msg);
+    }
+}
